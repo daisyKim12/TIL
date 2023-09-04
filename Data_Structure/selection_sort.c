d2faa52ecb3a54e3e06bfcec18e5d21c5e9856c0
Daisy <ksjmichael12@gmail.com>
2023-09-04 10:17:54 +0900

    selection sort study

---
+++ Data_Structure/selection_sort.c
@@ -0,0 +1,54 @@
+// selection sort
+/*
+Array is divided by sorted and unsorted parts
+And the objective is to expand sorted parts until there are no unsorted parts
+To expand sorted parts, unsorted parts finds the minimum value and switch the position with lowest index
+*/
+
+#include <stdio.h>
+
+void swap(int *n1, int *n2) {
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+    return 0;
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+
+    for(i = 0; i<n-1; i++) {
+        min_idx = i;
+        for(j = i + 1; j<n; j++){
+            if(arr[j] < arr[min_idx]){
+                min_idx = j;
+            }
+
+            if(min_idx != i) {
+                swap(&arr[min_idx], &arr[i]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+}
